0c9cba99d52be2fece292b91a333eac88e3978b2
andrei petrov <ndt.080@yandex.by>
2023-10-16 21:45:09 +0300

    feat: Article Page skeleton

--- src/pages/ArticlePage/ArticlePageSkeleton.tsx
+++ src/pages/ArticlePage/ArticlePageSkeleton.tsx
@@ -1,26 +1,40 @@
-import { PreviewCardSkeleton } from '@features/PreviewCard';
 import { Skeleton } from '@shared/ui/lib/skeleton.tsx';
+import { cn } from '@shared/utils';
 
 const SKELETON_ITEMS = Array.from({ length: 5 }, (_, i) => i);
 
 function ArticlePageSkeleton() {
   return (
-    <main className="bg-white h-full pt-2.5 flex flex-col gap-10">
-      <PreviewCardSkeleton className="h-[220px] md:h-[320px] mt-2.5" />
-      <div className="flex flex-col w-full gap-2.5">
-        {SKELETON_ITEMS.map((item) => (
-          <Skeleton key={item} className="w-full h-[20px]" />
-        ))}
-
-        <div className="w-full flex items-center justify-center p-5">
-          <Skeleton className="w-[50%] h-[300px]" />
+    <article className="bg-white w-full h-full pt-2.5 text-black flex flex-col items-center gap-10">
+      <Skeleton
+        className={cn(
+          'w-full min-h-[500px] !bg-cover !bg-article rounded-2xl',
+          'flex flex-col justify-end p-10',
+        )}
+      >
+        <div className="flex flex-col gap-2.5 w-full h-fit text-black lg:max-w-[75%] xl:max-w-[50%]">
+          <Skeleton className="h-[20px] w-[30%]" />
+          <Skeleton className="h-[40px] w-[50%]" />
+          <Skeleton className="h-[30px] w-[100%]" />
         </div>
+      </Skeleton>
 
-        {SKELETON_ITEMS.map((item) => (
-          <Skeleton key={item} className="w-full h-[20px]" />
-        ))}
-      </div>
-    </main>
+      <main className="w-full md:max-w-[85%]">
+        <div className="flex flex-col w-full gap-2.5">
+          {SKELETON_ITEMS.map((item) => (
+            <Skeleton key={item} className="w-full h-[20px]" />
+          ))}
+
+          <div className="w-full flex items-center justify-center p-5">
+            <Skeleton className="w-[50%] h-[300px]" />
+          </div>
+
+          {SKELETON_ITEMS.map((item) => (
+            <Skeleton key={item} className="w-full h-[20px]" />
+          ))}
+        </div>
+      </main>
+    </article>
   );
 }
 
