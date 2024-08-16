cc019fc27d058ba26384003b3a6eb01b3dade5b6
Andrey Petrov <ndt.080@yandex.by>
2024-08-16 09:22:47 +0300

    feat: add KungFu and PersonalGrowth pages

--- src/processes/navigation/routes/index.ts
+++ src/processes/navigation/routes/index.ts
@@ -4,9 +4,11 @@ import { RoutePath } from '../values/routePath.ts';
 
 export default [
   { path: RoutePath.Home, lazy: () => import('@pages/Home/HomePage.tsx') },
-  { path: RoutePath.Karate, lazy: () => import('@pages/BaseTextPage/BaseTextPage.tsx') },
-  { path: RoutePath.Schedule, lazy: () => import('@pages/BaseTextPage/BaseTextPage.tsx') },
-  { path: RoutePath.About, lazy: () => import('@pages/BaseTextPage/BaseTextPage.tsx') },
   { path: RoutePath.Article, lazy: () => import('@pages/ArticlePage/ArticlePage.tsx') },
   { path: RoutePath.Articles, lazy: () => import('@pages/ArticlesPage/ArticlesPage.tsx') },
+  { path: RoutePath.Karate, lazy: () => import('@pages/BaseTextPage/BaseTextPage.tsx') },
+  { path: RoutePath.KungFu, lazy: () => import('@pages/BaseTextPage/BaseTextPage.tsx') },
+  { path: RoutePath.PersonalGrowth, lazy: () => import('@pages/BaseTextPage/BaseTextPage.tsx') },
+  { path: RoutePath.Schedule, lazy: () => import('@pages/BaseTextPage/BaseTextPage.tsx') },
+  { path: RoutePath.About, lazy: () => import('@pages/BaseTextPage/BaseTextPage.tsx') },
 ] satisfies RouteObject[];
--- src/processes/navigation/values/routePath.ts
+++ src/processes/navigation/values/routePath.ts
@@ -2,6 +2,8 @@ export enum RoutePath {
   Home = '/',
   Karate = '/karate',
   Schedule = '/schedule',
+  PersonalGrowth = '/personal-growth',
+  KungFu = '/kung-fu',
   About = '/about',
   Articles = '/articles',
   Article = '/articles/:id',
